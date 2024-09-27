3f41a9429813bcfba3653fc50c9f13c03709c855
Kreoin <steamuser85@yandex.ru>
2024-09-27 19:29:23 +0300

    rename and modify engine.js to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { question } from 'readline-sync';
 const check = (qst, correctAnswer) => {
     console.log(`Question: ${qst}`);
     const answer = question('Your answer: ');
-    return answer === correctAnswer;
+    return answer == correctAnswer;
 }
 
-const message = (correct, name, answer, correctAnswer) => {
+const message = (correct, name, answer, correctAnswer, end) => {
     if (correct) {
         console.log("Correct!");
+        if (end === 3) {
+            console.log(`Congratulations, ${name}!`);
+        }
     } else {
         console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
         console.log(`Let's try again, ${name}!`);
