f8237209243344e0bdf7175de2f7ceeb818df670
Kreoin <steamuser85@yandex.ru>
2024-09-28 01:14:14 +0300

    rename imports

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import cli from "../src/cli.js";
-import gcd from "../src/games/gcd.js";
+import cli from '../src/cli.js';
+import gcd from '../src/games/gcd.js';
 
 const name = cli();
 gcd(name);
